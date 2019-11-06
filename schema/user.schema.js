1c6d8b2680cdd7b90047df442fc4ee07317e93c2
nicola <nicola.willis@canstudios.com>
2019-11-06 11:08:11 +0000

    Attempt to add db connection on boot

---
+++ schema/user.schema.js
@@ -0,0 +1,13 @@
+module.exports = {
+    name: 'user',
+    definition: {
+        email: {
+            type: 'String',
+            required: true
+        },
+        password: {
+            "type": "string",
+            "required": true
+        }
+    }
+};
